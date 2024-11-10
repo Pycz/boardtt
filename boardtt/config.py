3d05f1f1cf2854ecad3c00fcf21764d9d31a2bf4
Ruslan Borovilov <pycz2007@yandex.ru>
2024-11-10 19:53:34 +0100

    Refactor config

--- boardtt/config.py
+++ boardtt/config.py
@@ -1,63 +1,42 @@
-import logging
 from dataclasses import dataclass
-
-LOGGER = logging.getLogger(__name__)
-
-IMAGE_DPI = 600
-CARD_HEIGHT_MM = 88
-CARD_WIDTH_MM = 62
-CARDS_ROWS = 3
-CARDS_COLS = 3
-
-
-def configure(
-    image_dpi=600,
-    card_height_mm=88,
-    card_width_mm=62,
-    cards_rows=3,
-    cards_cols=3,
-    log_level=logging.INFO,
-):
-    """Конфигурирует приложение.
-
-    :param image_dpi: Ращерешение скана. Точки на дюйм.
-    :param card_height_mm: Высота карты, мм
-    :param card_width_mm: Ширина карты, мм
-    :param cards_rows: Кол-во рядов карт на скане
-    :param cards_cols: Кол-во столбцов карт на скане
-    :param log_level:
-    :return:
-    """
-    global IMAGE_DPI, CARD_HEIGHT_MM, CARD_WIDTH_MM, CARDS_ROWS, CARDS_COLS
-    # TODO Можно автоматизировать, скорее всего - автоматом вычислять ширину и высоту карты.
-
-    IMAGE_DPI = image_dpi
-    CARD_HEIGHT_MM = card_height_mm
-    CARD_WIDTH_MM = card_width_mm
-    CARDS_ROWS = cards_rows
-    CARDS_COLS = cards_cols
-
-    configure_logging(log_level)
-
-
-def configure_logging(log_level=logging.INFO, show_logger_names=False):
-    """Performs basic logging configuration.
-
-    :param log_level: logging level, e.g. logging.DEBUG
-    :param show_logger_names: bool - flag to show logger names in output
-    :return:
-    """
-    format_str = "%(levelname)s: %(message)s"
-    if show_logger_names:
-        format_str = "%(name)s\t\t " + format_str
-    logging.basicConfig(format=format_str, level=log_level)
 
 
 @dataclass
 class Config:
-    image_dpi: int = 600
-    card_height_mm: int | float = 88
-    card_width_mm: int | float = 62
-    cards_rows: int = 3
-    cards_cols: int = 3
-    log_level: int = logging.INFO
+    cards_rows: int  # сколько рядов карт на скане
+    cards_cols: int  # сколько столбцов карт на скане
+
+    image_dpi: int = 300
+    offset_from_top_border_mm: int | float = 0   # сколько миллиметров от верхнего края до первой карточки
+    offset_from_left_border_mm: int | float = 0  # сколько миллиметров от левого края до первой карточки
+    card_height_mm: int | float = 80  # высота карты в миллиметрах
+    card_width_mm: int | float = 60  # ширина карты в миллиметрах
+    offset_x_mm: int | float = 1  # отступ между картами по горизонтали
+    offset_y_mm: int | float = 1  # отступ между картами по вертикали
+
+    def _mm_to_pixels(self, mm: int | float) -> int:
+        return int((self.image_dpi * mm) / 25.4)
+
+    @property
+    def card_height_px(self) -> int:
+        return self._mm_to_pixels(self.card_height_mm)
+
+    @property
+    def card_width_px(self) -> int:
+        return self._mm_to_pixels(self.card_width_mm)
+
+    @property
+    def offset_x_px(self) -> int:
+        return self._mm_to_pixels(self.offset_x_mm)
+
+    @property
+    def offset_y_px(self) -> int:
+        return self._mm_to_pixels(self.offset_y_mm)
+
+    @property
+    def offset_from_top_border_px(self) -> int:
+        return self._mm_to_pixels(self.offset_from_top_border_mm)
+
+    @property
+    def offset_from_left_border_px(self) -> int:
+        return self._mm_to_pixels(self.offset_from_left_border_mm)
