0bf46f2bd897ea85ee1b47690eb1b3efc0ce6dad
Ruslan Borovilov <pycz2007@yandex.ru>
2024-11-10 19:54:25 +0100

    Make proper planar marker

--- boardtt/marker.py
+++ boardtt/marker.py
@@ -3,8 +3,8 @@ from typing import TypedDict, Protocol
 
 from PIL import Image
 
-from boardtt.config import CARD_HEIGHT_MM, CARD_WIDTH_MM, CARDS_COLS, CARDS_ROWS, LOGGER
-from boardtt.utils import mm_to_pixels
+from boardtt.config import Config
+from boardtt.logger import LOGGER
 
 
 class CardData(TypedDict):
@@ -16,76 +16,50 @@ CardsData = list[CardData]
 
 
 class CardMarker(Protocol):
-    def get_cards(self, img: Image) -> CardsData:
+    def get_cards(self) -> CardsData:
         ...
 
 
 class PlanarCardMarker(CardMarker):
-    def __init__(self, filepath: str | os.PathLike):
+    def __init__(self, config: Config, filepath: str | os.PathLike):
         self.filepath = filepath
-
-    def get_cards(self, img: Image) -> CardsData:
-        ...
+        self.config = config
 
 
-def open_image_file(fpath):
-    """Возвращает открытый Pillow файл с изображением.
-
-    :param fpath:
-    :return:
-    """
-    return Image.open(fpath)
+    def _open_image_file(self) -> Image:
+        """Возвращает открытый Pillow файл с изображением.
+        """
+        return Image.open(self.filepath)
 
 
-def get_card_coords(row_num, col_num):
-    """Возвращает координаты карты по её расположению в ряду, колонке.
+    def _get_card_coords(self, row_num: int, col_num: int) -> tuple[int, int, int, int]:
+        """Возвращает координаты карты по её расположению в ряду, колонке.
+        """
 
-    :param row_num: Номер ряда
-    :param col_num: Номер колонки
-    :return:
-    """
+        x = self.config.offset_from_left_border_px + (row_num * (self.config.card_width_px + self.config.offset_x_px))
+        x1 = x + self.config.card_width_px
 
-    CARD_HEIGHT_PX = mm_to_pixels(CARD_HEIGHT_MM)
-    CARD_WIDTH_PX = mm_to_pixels(CARD_WIDTH_MM)
+        y = self.config.offset_from_top_border_px + (col_num * (self.config.card_height_px + self.config.offset_y_px))
+        y1 = y + self.config.card_height_px
 
-    OFFSET_X_PX = 2
-    OFFSET_Y_PX = 2
-
-    x = row_num * CARD_WIDTH_PX + OFFSET_X_PX
-    x1 = x + CARD_WIDTH_PX
-
-    y = col_num * CARD_HEIGHT_PX + OFFSET_Y_PX
-    y1 = y + CARD_HEIGHT_PX
-
-    return x, y, x1, y1
+        return x, y, x1, y1
 
 
-def get_cards(img):
-    """Возвращает список с данными карт с указанного изображения (скана).
+    def get_cards(self) -> CardsData:
+        """Возвращает список с данными карт с указанного изображения (скана).
+        """
+        LOGGER.info("Loading cards from %s" % self.filepath)
+        img = self._open_image_file()
 
-    :param img: объект изображения от Pillow
-    :return:
-    """
-    cards = []
+        cards = []
 
-    for col_num in range(CARDS_COLS):
-        for row_num in range(CARDS_ROWS):
-            LOGGER.debug("Getting card %sx%s ..." % (col_num + 1, row_num + 1))
-            coords = get_card_coords(row_num, col_num)
-            card = img.crop(coords)
-            cards.append({"img": card, "coords": coords})
+        for col_num in range(self.config.cards_cols):
+            for row_num in range(self.config.cards_rows):
+                LOGGER.debug("Getting card %s x %s ..." % (col_num + 1, row_num + 1))
+                coords = self._get_card_coords(row_num, col_num)
+                card = img.crop(coords)
+                cards.append({"img": card, "coords": coords})
 
-    LOGGER.info("Source image split into %s cards" % len(cards))
+        LOGGER.info("Source image split into %s cards" % len(cards))
 
-    return cards
-
-
-def get_cards_from_file(fpath):
-    """Возвращает данные о картах, изъятых из указанно файла-скана.
-
-    :param fpath:
-    :return:
-    """
-    LOGGER.info("Loading cards from %s" % fpath)
-    img = open_image_file(fpath)
-    return get_cards(img)
+        return cards
